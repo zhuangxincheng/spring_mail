9a65861060e16b73e41b399204e1cde06b8328f9
zhuangxincheng <zhuangxincheng@LAPTOP-HHDMRH50>
2018-06-20 15:15:20 +0800

    修改

---
+++ src/main/java/com/LoggerAopConfig.java
@@ -0,0 +1,55 @@
+package com;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+import com.alibaba.fastjson.JSON;
+
+@Aspect
+@Configuration
+public class LoggerAopConfig {
+	 private static final Logger log = LoggerFactory.getLogger(LoggerAopConfig.class);
+
+	    // 只关注方法名为find前缀的
+	    @Pointcut("execution(* com.demo..*.*(..))")
+	    public void executeService() {}
+
+
+	    @Before("executeService()")
+	    public void invokeBefore(JoinPoint point) {
+	        String realClassName = getRealClassName(point);
+	        Object a = point.getArgs()[0];
+	        log.info("调用-----"+ realClassName + " 执行 " + getMethodName(point) + " 方法之前"+"--参数"+JSON.toJSONString(a));
+	    }
+
+	    @After("executeService()")
+	    public void invokeAfter(JoinPoint point) {
+	        String realClassName = getRealClassName(point);
+	        log.info("调用-----"+ realClassName + " 执行 " + getMethodName(point) + " 方法之后");
+	    }
+
+
+	    /**
+	     * 获取被代理对象的真实类全名
+	     * @param point 连接点对象
+	     * @return 类全名
+	     */
+	    private String getRealClassName(JoinPoint point) {
+	        return point.getTarget().getClass().getName();
+	    }
+
+	    /**
+	     * 获取代理执行的方法名
+	     * @param point 连接点对象
+	     * @return 调用方法名
+	     */
+	    private String getMethodName(JoinPoint point) {
+	        return point.getSignature().getName();
+	    }
+}
--- src/main/java/com/demo/controller/mailTest.java
+++ src/main/java/com/demo/controller/mailTest.java
@@ -1,14 +1,30 @@
 package com.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 @SuppressWarnings("all")
 public class mailTest {
 	public static void main(String[] args) {
-		List<String> aa = new ArrayList<String>();
-		aa.add(null);
-		aa.add("zxc");
-		System.out.println(aa.contains(null));
+		Date date = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		long ago = 0L;
+		try {
+			 ago = format.parse("2018-03-21").getTime();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long a = date.getTime();
+		System.out.println(a>ago);
+		
+//		List<String> aa = new ArrayList<String>();
+//		aa.add("123");
+//		aa.add("zxc");
+//		aa.add("456");
+//		aa.add("789");
+//		System.out.println(aa.subList(2, 4));
+		//C:\Program Files\Java\jdk1.8.0_45\bin
 		
 		
 //		User user = new User();
