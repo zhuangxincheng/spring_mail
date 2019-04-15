2981398182f742f75a9d6447030e64b6d25b0607
zhuangxincheng <18310763990@163.com>
2019-04-15 16:40:08 +0800

    格式化

--- src/main/java/com/demo/controller/lambdaTestController.java
+++ src/main/java/com/demo/controller/lambdaTestController.java
@@ -1,10 +1,5 @@
 package com.demo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,36 +7,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/springboot123/")
 @SuppressWarnings("all")
 public class lambdaTestController {
-	private static Logger logger = LoggerFactory.getLogger(lambdaTestController.class);
-	
-	@RequestMapping(value = "lambdaTestController", method = RequestMethod.GET)
-	@Test
-	public void sendMail() {
-		List<String> test = new ArrayList<String>();
-		Optional.ofNullable(null).map(m ->{
-			return false;
-		}).orElse(true);
-		System.out.println("xxx");
-	}
-	
-	@Test
+    private static Logger logger = LoggerFactory.getLogger(lambdaTestController.class);
+
+    @RequestMapping(value = "lambdaTestController", method = RequestMethod.GET)
+    @Test
+    public void sendMail() {
+        List<String> test = new ArrayList<String>();
+        Optional.ofNullable(null).map(m -> {
+            return false;
+        }).orElse(true);
+        System.out.println("xxx");
+    }
+
+    @Test
     public void test14() {
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
         list.add(3);
-        
+
         List<String> strs = new ArrayList<String>();
         strs.add("一");
         strs.add("二");
         strs.add("三");
         String str = String.join(",", strs);
-        
-        list =  list.stream().filter(l -> l>2).collect(Collectors.toList());
+
+        list = list.stream().filter(l -> l > 2).collect(Collectors.toList());
         // 可改变对象
         list = list.stream().map((i) -> i * 3).collect(Collectors.toList());
 
