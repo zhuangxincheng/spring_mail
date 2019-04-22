fe5232b29c476556f649e6d70badc9fb6003a45b
zhuangxincheng <18310763990@163.com>
2019-04-22 17:03:52 +0800

    解决mapper注入报错

--- src/main/java/com/mail/mapping/UserMapperExt.java
+++ src/main/java/com/mail/mapping/UserMapperExt.java
@@ -3,10 +3,12 @@ package com.mail.mapping;
 import com.mail.model.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Mapper
+@Component
 public interface UserMapperExt extends UserMapper{
     @Select("select  username, password  from user where 1=1")
     List<User> selectByEId(User user);
