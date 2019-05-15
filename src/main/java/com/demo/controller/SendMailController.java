bf169de1d3a5ea5a8e16e9fa2077445bbcd0798f
zhuangxincheng <18310763990@163.com>
2019-05-15 14:49:24 +0800

    格式化

--- src/main/java/com/demo/controller/SendMailController.java
+++ src/main/java/com/demo/controller/SendMailController.java
@@ -23,32 +23,32 @@ import java.util.concurrent.ExecutionException;
 @RequestMapping("")
 @Api(value = "SendMailController", description = "邮件发送模块")
 public class SendMailController {
-	private static Logger logger = LoggerFactory.getLogger(SendMailController.class);
-	@Autowired
-	sendMailUtil sendMailUtil;
-	@Autowired
-	Task Task;
-	@Autowired
-	UserMapperExt mapper;
-	@Autowired
-	ApplicationContext applicationContext;
+    private static Logger logger = LoggerFactory.getLogger(SendMailController.class);
+    @Autowired
+    sendMailUtil sendMailUtil;
+    @Autowired
+    Task Task;
+    @Autowired
+    UserMapperExt mapper;
+    @Autowired
+    ApplicationContext applicationContext;
 
-	@ApiOperation(value = "邮件发送", notes = "通过javamailSender")
-	@RequestMapping(value = "sendMail", method = RequestMethod.GET)
-	public void sendMail(TestBean test) throws InterruptedException, ExecutionException {
-		String[] email = new String[] { "xincheng.zhuang@wowjoy.cn", "ceshi1@wowjo.cn", "ceshi2@wowjoy.cn",
-				"ceshi3@wowjoy.cn" };
-		String title = "測試";
-		String text = "測試";
-		String html = "<html><head>" + "</body></html>";
-		long start = System.currentTimeMillis();
-		int code = 0;
+    @ApiOperation(value = "邮件发送", notes = "通过javamailSender")
+    @RequestMapping(value = "sendMail", method = RequestMethod.GET)
+    public void sendMail(TestBean test) throws InterruptedException, ExecutionException {
+        String[] email = new String[]{"xincheng.zhuang@wowjoy.cn", "ceshi1@wowjo.cn", "ceshi2@wowjoy.cn",
+                "ceshi3@wowjoy.cn"};
+        String title = "測試";
+        String text = "測試";
+        String html = "<html><head>" + "</body></html>";
+        long start = System.currentTimeMillis();
+        int code = 0;
 
 //		List<Future<String>> list = new ArrayList<Future<String>>();
 //		Future<String> tatsk = null;
 //		for (int i = 0; i < email.length; i++) {
 //
-			sendMailUtil.sendHtml("xincheng.zhuang@wowjoy.cn", "18310763990Zxc", title, html, email[0]);
+        sendMailUtil.sendHtml("xincheng.zhuang@wowjoy.cn", "18310763990Zxc", title, html, email[0]);
 //
 //			list.add(tatsk);
 //
@@ -60,47 +60,47 @@ public class SendMailController {
 //
 //		}
 
-		// while (true) {
-		// String aaa = list.get(list.size() - 1).get();
-		// if (list.get(list.size() - 1).get().equals("200")) {
-		// Thread.sleep(500);
-		// break;
-		// }
-		//
-		// }
-		// for(Future<String> feture : list) {
-		// if(feture.isDone()) {
-		// flag = true;
-		// }else {
-		// flag = false;
-		// }
-		// }
-		long end = System.currentTimeMillis();
-		System.out.println("耗时：" + (end - start) + "毫秒");
-	}
+        // while (true) {
+        // String aaa = list.get(list.size() - 1).get();
+        // if (list.get(list.size() - 1).get().equals("200")) {
+        // Thread.sleep(500);
+        // break;
+        // }
+        //
+        // }
+        // for(Future<String> feture : list) {
+        // if(feture.isDone()) {
+        // flag = true;
+        // }else {
+        // flag = false;
+        // }
+        // }
+        long end = System.currentTimeMillis();
+        System.out.println("耗时：" + (end - start) + "毫秒");
+    }
 
-	@ApiOperation(value = "用户信息获取", notes = "用户信息获取")
-	@ApiImplicitParam(name = "userId", value = "用户ID", paramType = "query", required = true, dataType = "String")
-	@RequestMapping(value = "user", method = RequestMethod.GET)
-	public List<User> getUserOnfo(String userId) {
-		List<User> users = mapper.selectByEId(null);
-		DataSource dataSource = applicationContext.getBean(DataSource.class);
-		// 查看配置数据源信息
-		System.out.println(dataSource);
-		System.out.println(dataSource.getClass().getName());
-		return users;
-	}
+    @ApiOperation(value = "用户信息获取", notes = "用户信息获取")
+    @ApiImplicitParam(name = "userId", value = "用户ID", paramType = "query", required = true, dataType = "String")
+    @RequestMapping(value = "user", method = RequestMethod.GET)
+    public List<User> getUserOnfo(String userId) {
+        List<User> users = mapper.selectByEId(null);
+        DataSource dataSource = applicationContext.getBean(DataSource.class);
+        // 查看配置数据源信息
+        System.out.println(dataSource);
+        System.out.println(dataSource.getClass().getName());
+        return users;
+    }
 
-	@RequestMapping(value = "testTask", method = RequestMethod.GET)
-	public void testTask() {
-		Task.doTaskOne();
-		Task.doTaskTwo();
-	}
-	
-	@RequestMapping(value = "testTask", method = RequestMethod.PUT)
-	public void testTaskEdit() {
-		Task.doTaskOne();
-		Task.doTaskTwo();
-	}
+    @RequestMapping(value = "testTask", method = RequestMethod.GET)
+    public void testTask() {
+        Task.doTaskOne();
+        Task.doTaskTwo();
+    }
+
+    @RequestMapping(value = "testTask", method = RequestMethod.PUT)
+    public void testTaskEdit() {
+        Task.doTaskOne();
+        Task.doTaskTwo();
+    }
 
 }
