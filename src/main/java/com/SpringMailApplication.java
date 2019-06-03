cadd7261fabc7441c14fe2afc7fff152325365a8
zhuangxincheng <18310763990@163.com>
2019-06-03 10:44:45 +0800

    mapper扫描

--- src/main/java/com/SpringMailApplication.java
+++ src/main/java/com/SpringMailApplication.java
@@ -1,11 +1,13 @@
 package com;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan(basePackages = "com.mail.mapping")
 //@EnableDiscoveryClient
 public class SpringMailApplication {
 
